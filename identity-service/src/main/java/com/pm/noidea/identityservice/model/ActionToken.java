a2138bb700d441442720a76717f85d7666c2fb15
Margiorno <m.zawiski03@gmail.com>
2025-09-14 19:18:20 +0200

    action token class made

---
+++ identity-service/src/main/java/com/pm/noidea/identityservice/model/ActionToken.java
@@ -0,0 +1,33 @@
+package com.pm.noidea.identityservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class ActionToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    @ManyToOne(fetch = FetchType.EAGER)
+    private AuthUser user;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ActionType actionType;
+
+    @Column(nullable = false)
+    private LocalDateTime expires;
+}

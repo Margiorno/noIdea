be35ba170acd3bc4c94cb76a5e20cb4293429280
Margiorno <m.zawiski03@gmail.com>
2025-09-15 12:41:22 +0200

    email sending works

--- notification-service/src/test/java/com/pm/noidea/notificationservice/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/pm/noidea/notificationservice/NotificationServiceApplicationTests.java
@@ -1,13 +1,19 @@
 package com.pm.noidea.notificationservice;
 
+import com.pm.noidea.notificationservice.service.EmailService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NotificationServiceApplicationTests {
 
+    @Autowired
+    private EmailService emailService;
+
     @Test
-    void contextLoads() {
+    void emailSendToMe() {
+        emailService.sendVerificationCode("m.zawiski03@gmail.com","test");
     }
 
 }

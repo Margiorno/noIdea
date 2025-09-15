02f60175ba3bf8c4c50fe2f9ef615ad6fab264d3
Margiorno <m.zawiski03@gmail.com>
2025-09-15 11:59:15 +0200

    notification-service init

---
+++ notification-service/src/main/java/com/pm/noidea/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.noidea.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+}

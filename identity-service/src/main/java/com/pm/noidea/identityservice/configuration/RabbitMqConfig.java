1ad59c6ec0d640e81d8b30b2dc1a9ad298c15d24
Margiorno <m.zawiski03@gmail.com>
2025-09-16 22:02:39 +0200

    improved communication config

--- identity-service/src/main/java/com/pm/noidea/identityservice/configuration/RabbitMqConfig.java
+++ identity-service/src/main/java/com/pm/noidea/identityservice/configuration/RabbitMqConfig.java
@@ -1,29 +1,30 @@
 package com.pm.noidea.identityservice.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class RabbitMqConfig {
 
-    public static final String REGISTERED_EVENT_TOPIC = "registered-queue";
-    public static final String EXCHANGE = "notification-exchange";
-    public static final String REGISTER_EVENT_ROUTING_KEY = "registered-event";
+    private final RabbitMqProperties rabbitMqProperties;
 
     @Bean
     public Queue registeredEventQueue() {
-        return new Queue(REGISTERED_EVENT_TOPIC);
+        return new Queue(rabbitMqProperties.getRegisteredEventTopic());
     }
 
     @Bean
     public DirectExchange exchange() {
-        return new DirectExchange(EXCHANGE);
+        return new DirectExchange(rabbitMqProperties.getExchangeName());
     }
 
     @Bean
@@ -33,7 +34,7 @@ public class RabbitMqConfig {
         return BindingBuilder
                 .bind(queue)
                 .to(exchange)
-                .with(REGISTER_EVENT_ROUTING_KEY);
+                .with(rabbitMqProperties.getRegisteredEventRoutingKey());
     }
 
     @Bean

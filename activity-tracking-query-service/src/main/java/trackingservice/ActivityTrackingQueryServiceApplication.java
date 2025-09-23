dff8474b4761d1939d157da24c5bfae57aafbd6d
Margiorno <m.zawiski03@gmail.com>
2025-09-23 15:25:47 +0200

    activity tracking query side initiallized

---
+++ activity-tracking-query-service/src/main/java/trackingservice/ActivityTrackingQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package trackingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityTrackingQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityTrackingQueryServiceApplication.class, args);
+	}
+
+}
